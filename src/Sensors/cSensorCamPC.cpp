e8bb303920a5d580a0acebbd4b16c9f5fc763409
Ewelina Rupnik <erupnik@gmail.com>
2025-06-12 20:23:56 +0200

    Add BA on bundles V1

--- src/Sensors/cSensorCamPC.cpp
+++ src/Sensors/cSensorCamPC.cpp
@@ -214,6 +214,11 @@ cPt3dr  cSensorCamPC::EpsDiffGround2Im(const cPt3dr & aPt) const
 
 cPt2dr cSensorCamPC::Ground2Image(const cPt3dr & aP) const
 {
+     if (mInternalCalib==nullptr)
+    {
+         cPt3dr aPCam = Pt_W2L(aP);
+         return cPt2dr(aPCam.x()/aPCam.z(),aPCam.y()/aPCam.z());
+    }
      return mInternalCalib->Value(Pt_W2L(aP));
 }
 
@@ -304,7 +309,7 @@ const cPt3dr * cSensorCamPC::CenterOfPC() const { return  & Center(); }
 cCalculator<double> * cSensorCamPC::CreateEqColinearity(bool WithDerives,int aSzBuf,bool ReUse)
 {
    if (mInternalCalib==nullptr)
-      return nullptr;
+      return EqColinearityCamProj(WithDerives,aSzBuf,ReUse);//nullptr;
    return mInternalCalib->EqColinearity(WithDerives,aSzBuf,ReUse);
 }
 
